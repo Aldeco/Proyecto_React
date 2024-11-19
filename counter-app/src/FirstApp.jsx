74852443982265bc6def314068884d3a6f127680
laldeco <laldeco@fxinformatica.com>
2024-11-18 22:47:31 -0300

    Clase 39. Explicación de <Parameter>, además de devolver Funciones pero no objetos { }.

--- counter-app/src/FirstApp.jsx
+++ counter-app/src/FirstApp.jsx
@@ -1,5 +1,13 @@
+const getFullName = (nombre, apellido) => {
+
+  return nombre + " " + apellido;
+}
+
 export const FirstApp = () => {
   return (
-    <h1>¡FirstApp!</h1>
+    <>
+      <h1> { getFullName("Nico", "Galeano") } </h1>
+      <code> código </code>
+    </>
   )
 }
