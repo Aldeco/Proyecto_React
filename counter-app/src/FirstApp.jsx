8b502c5eebcbdd964e5ff62b9a2e2d584bbb641b
laldeco <laldeco@fxinformatica.com>
2024-11-19 03:13:08 -0300

    jugando un poco con el ejercicio de la clase 39.

--- counter-app/src/FirstApp.jsx
+++ counter-app/src/FirstApp.jsx
@@ -1,13 +1,64 @@
-const getFullName = (nombre, apellido) => {
+// const getFullName = (nombre, apellido) => {
 
-  return nombre + " " + apellido;
-}
+//   return nombre + " " + apellido;
+// }
+
+// export const FirstApp = () => {
+
+//   return (
+//     <>
+//       <h1> { getFullName("Nico", "Galeano") } </h1>
+//       <code> código </code>
+//     </>
+//   )
+// }
+
+
+// Esto no es del curso!
+import React, { useState } from 'react';
 
 export const FirstApp = () => {
+
+  const [formState, setFormState] = useState({
+    
+    nombre: '',
+    apellido: '',
+  });
+
+  const handleInputChange = (e) => {
+    
+    const { name, value } = e.target;
+    
+    setFormState({
+    
+      ...formState,
+      [name]: value, 
+    });
+  };
+
+  const getFullName = () => `${formState.nombre} ${formState.apellido}`;
+
   return (
     <>
-      <h1> { getFullName("Nico", "Galeano") } </h1>
-      <code> código </code>
+      <h1>{getFullName()}</h1>
+
+      <form>
+        <div>
+          <label> 
+            Nombre:
+            <input type="text" name="nombre" value={formState.nombre} onChange={handleInputChange}/>
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Apellido:
+            <input type="text" name="apellido" value={formState.apellido} onChange={handleInputChange}/>
+          </label>
+        </div>
+      </form>
+
+      <code>código</code>
     </>
-  )
-}
+  );
+};
